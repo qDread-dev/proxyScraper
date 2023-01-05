a8c21eee35d36afdfd6b4b1122902f1c17391160
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-01-05 16:07:48 +0000

    added timeout

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ url = "https://api.proxyscrape.com/v2/?request=proxyinfo"
 headers={}
 payload={}
 
-response = requests.request(GET, url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers, data=payload,timeout=5)
 print(response.text)
