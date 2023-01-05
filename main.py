b56fed1432b6f27a8938a9bf208c988fabeba0ff
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-01-05 16:06:23 +0000

    Basic Update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
 
+url = "https://api.proxyscrape.com/v2/?request=proxyinfo"
+
+headers={}
+payload={}
+
+response = requests.request(GET, url, headers=headers, data=payload)
+print(response.text)
